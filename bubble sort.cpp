4f082d51f703eb49e7efaf0632f9291eff2d6ea0
mahodi56 <mahodimahtabiiuc@gmail.com>
2025-02-21 16:09:44 +0600

    Bubble sort

---
+++ bubble sort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)  // If no swaps occurred, the array is already sorted
+            break;
+    }
+
+    // Additional Bubble Sort pass (Your logic)
+    for (int i = n - 1; i > 1; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Main function
+int main() {
+    int n;
+
+    // Taking array size input
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n]; // Dynamic array
+
+    // Taking array elements input
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+
+    // Sorting the array using Bubble Sort
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
