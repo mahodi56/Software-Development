5f874efbba89f3b33abe4b18c84decc2fe9ae6be
mahodi56 <mahodimahtabiiuc@gmail.com>
2025-03-17 22:37:39 +0600

    Delete from a linked list

---
+++ linked_list(delete after a node).cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct linked_list{
+    int info;
+    struct linked_list *address;
+};
+typedef struct linked_list node;
+int main()
+{
+    node *start,*ptr,*new_01,*new_02;
+    start=(node*)malloc(sizeof(node));
+    ptr=start;
+    int n,value;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>ptr->info;
+        if(i!=n){
+            ptr->address=(node*)malloc(sizeof(node));
+            ptr=ptr->address;
+        }
+    }
+    ptr->address=NULL;
+    cin>>value;
+    new_01=start;
+    new_02=new_01->address;
+    while(new_02->info!=value && new_02->address!=NULL){
+        new_01=new_01->address;
+        new_02=new_02->address;
+
+    }
+    if(new_02->info==value){
+       new_01->address=new_02->address;
+    free(new_02);
+    }
+    else
+        cout<<"The iteam not found"<<endl;
+
+    ptr=start;
+    while(ptr!=NULL){
+        cout<<ptr->info<<" ";
+        ptr=ptr->address;
+    }
+    return 0;
+}
+
+
